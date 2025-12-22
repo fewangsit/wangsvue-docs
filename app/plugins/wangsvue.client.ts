ac87cf00135b513907cae88f13921ef3324696c7
Zain Kurnia <programmer-fe3@qwertysystem.net>
2025-12-22 16:56:27 +0700

    style(wangsvue): remove unnecessary type annotation from nuxtApp parameter
    - Remove explicit `unknown` type annotation from nuxtApp parameter
    - Rely on TypeScript's type inference for cleaner code
    - Improve code readability by eliminating redundant type casting

--- app/plugins/wangsvue.client.ts
+++ app/plugins/wangsvue.client.ts
@@ -1,7 +1,7 @@
 import { Focus, Tooltip, WangsVue } from '@fewangsit/wangsvue'
 import preset from '@fewangsit/wangsvue-presets/wangsvue'
 
-export default defineNuxtPlugin((nuxtApp: unknown) => {
+export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(WangsVue, { preset })
   nuxtApp.vueApp.directive('Tooltip', Tooltip)
   nuxtApp.vueApp.directive('focus', Focus)
